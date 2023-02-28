63f5ec38595c67fe0a0252d1b931bf96809e6ac4
you@example.com <you@example.com>
2023-02-28 15:48:48 +0100

    comment

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import factorymethod.DogFactory;
 import factorymethod.TigerFactory;
 
 /**
- * Factory Method is a creational design pattern
+ * Factory Method is a creational design pattern,
  * that provides an interface for creating objects in a superclass,
  * but allows subclasses to alter the type of objects that will be created.
  */
